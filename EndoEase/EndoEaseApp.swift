1269c7ae8229ddaadc1f138cd700271422525a33
Saranya Mandapaty <saranya.broadway@gmail.com>
2023-06-27 14:13:17 -0700

    Initial Commit

---
+++ EndoEase/EndoEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndoEaseApp.swift
+//  EndoEase
+//
+//  Created by Saranya Mandapaty on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct EndoEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
